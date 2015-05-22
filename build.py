00ef14440aed67c632898dcfbbb8db86a38afd65
Arne Hilmann <arne@hilmann.de>
2015-05-23 00:45:42 +0200

    add dependencies

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
+use_plugin("python.frosted")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 
@@ -14,4 +15,7 @@ default_task = "publish"
 
 @init
 def set_properties(project):
-    pass
+    project.build_depends_on("mock")
+
+    project.set_property("flake8_verbose_output", True)
+    project.set_property("flake8_break_build", True)
